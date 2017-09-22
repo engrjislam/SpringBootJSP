47fe2c7d1a8c0623ec2e023cdb957da0bc3a9f96
jislan2510 <johirul.islam@engineer.com>
2017-09-22 18:24:14 +0300

    primary setup

---
+++ src/main/java/com/maxpro/SpringBootJSPApplication.java
@@ -0,0 +1,13 @@
+package com.maxpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJSPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJSPApplication.class, args);
+	}
+
+}
